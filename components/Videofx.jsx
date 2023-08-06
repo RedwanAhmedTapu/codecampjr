9e61cbecf24871a1455ba7f7c44a45013a51771e
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-08-06 16:19:05 +0600

    some line delted

--- components/Videofx.jsx
+++ components/Videofx.jsx
@@ -1,62 +1,70 @@
-"use client";
-import { React, useEffect, useRef,useMemo } from "react";
-// import Introvideos from "../video/circuit1.mp4";
-
-const Videofx = () => {
-  const videoRef = useRef();
-
-  const eventHandle = () => {
-    videoRef.current &&  videoRef.current.play();
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", eventHandle);
-  }, []);
-  return (
-    <>
-    {
-      
-      <video
-        autoPlay
-        loop
-        ref={videoRef}
-        src="/circuit1.mp4"
-        poster="/assets/images/fullBlackDiv.png"
-        className="bg_color"
-      ></video>
-    }
-    </>
-  );
-};
-
-export default Videofx;
-
 // "use client";
-// import { React, useEffect, useRef } from "react";
+// import { React, useEffect, useRef,useMemo } from "react";
 // // import Introvideos from "../video/circuit1.mp4";
 
 // const Videofx = () => {
 //   const videoRef = useRef();
-//   videoRef.current && videoRef.current.play();
+
 //   const eventHandle = () => {
-//     // if (typeof videoRef.current.play() !== undefined) videoRef.current.play();
-//     // 
+//     videoRef.current &&  videoRef.current.play();
 //   };
-//   // useEffect(() => {
-//   //   window.addEventListener("scroll", eventHandle);
-//   // }, []);
-//   // setTimeout(eventHandle, Infinity);
+//   useEffect(() => {
+//     window.addEventListener("scroll", eventHandle);
+//   }, []);
 //   return (
 //     <>
+//     {
+      
 //       <video
 //         autoPlay
 //         loop
 //         ref={videoRef}
 //         src="/circuit1.mp4"
-//         controls="false"
+//         poster="/assets/images/fullBlackDiv.png"
 //         className="bg_color"
 //       ></video>
+//     }
 //     </>
 //   );
 // };
 
 // export default Videofx;
+
+// // "use client";
+// // import { React, useEffect, useRef } from "react";
+// // // import Introvideos from "../video/circuit1.mp4";
+
+// // const Videofx = () => {
+// //   const videoRef = useRef();
+// //   videoRef.current && videoRef.current.play();
+// //   const eventHandle = () => {
+// //     // if (typeof videoRef.current.play() !== undefined) videoRef.current.play();
+// //     // 
+// //   };
+// //   // useEffect(() => {
+// //   //   window.addEventListener("scroll", eventHandle);
+// //   // }, []);
+// //   // setTimeout(eventHandle, Infinity);
+// //   return (
+// //     <>
+// //       <video
+// //         autoPlay
+// //         loop
+// //         ref={videoRef}
+// //         src="/circuit1.mp4"
+// //         controls="false"
+// //         className="bg_color"
+// //       ></video>
+// //     </>
+// //   );
+// // };
+
+// // export default Videofx;
+
+const Videofx = () => {
+  return (
+    <div>Videofx</div>
+  )
+}
+
+export default Videofx
