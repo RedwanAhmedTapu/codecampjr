a591f2404a5a69490f1314d467365431dadef019
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-17 09:39:49 +0600

    sign in with google

--- components/GoogleAuth.jsx
+++ components/GoogleAuth.jsx
@@ -1,28 +1,27 @@
 // GoogleAuth.js
 "use client"
 import React, { useState } from 'react';
-import { useGoogleOAuth } from 'react-oauth/google';
-import { GOOGLE_CLIENT_ID, REDIRECT_URI } from './config';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 
 function GoogleAuth() {
-  const { signIn, signOut, user, error } = useGoogleOAuth({
-    clientId: "88978267974-960jdldi46iudai4ludisduoi9h2jbuk.apps.googleusercontent.com",
-    redirectUri: `https://codecampjr.vercel.app/select-level`,
-  });
+  // const { signIn, signOut, user, error } = useGoogleOAuth({
+  //   clientId: "88978267974-960jdldi46iudai4ludisduoi9h2jbuk.apps.googleusercontent.com",
+  //   redirectUri: `https://codecampjr.vercel.app/select-level`,
+  // });
 
   return (
     <div>
-      {user ? (
-        <div>
-          <p>Welcome, {user.name}</p>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={signIn}>Sign In with Google</button>
-          {error && <p>Error: {error.message}</p>}
-        </div>
-      )}
+    <GoogleOAuthProvider clientId="<88978267974-960jdldi46iudai4ludisduoi9h2jbuk.apps.googleusercontent.com>">
+    <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>;
+    </GoogleOAuthProvider>;
     </div>
   );
 }
