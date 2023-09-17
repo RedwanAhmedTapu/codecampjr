42aa6524c13ab431c167179553cd01b039d40ef8
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-17 12:35:19 +0600

    jwt_decode added

--- components/GoogleAuth.jsx
+++ components/GoogleAuth.jsx
@@ -2,15 +2,18 @@
 "use client";
 import React, { useState } from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GoogleLogin,useGoogleOAuth } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 function GoogleAuth() {
   // const { signIn, signOut, user, error } = useGoogleOAuth({
   //   clientId: "88978267974-960jdldi46iudai4ludisduoi9h2jbuk.apps.googleusercontent.com",
   //   redirectUri: `https://codecampjr.vercel.app/select-level`,
   // });
-  const { signIn, signOut, user, error } = useGoogleOAuth();
-  console.log(user)
+  console.log(user);
+  var decoded = jwt_decode(token);
+ 
+console.log(decoded);
 
   return (
     <GoogleOAuthProvider clientId="652975357008-sut0t0e8g66jbjaqbnouk0im5ofi3a5o.apps.googleusercontent.com">
