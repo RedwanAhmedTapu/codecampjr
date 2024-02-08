594a333de2538733711bed8482f87bf1863cc84f
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-02-09 02:15:38 +0600

    nav.js updated

--- components/HoverableDropdown.jsx
+++ components/HoverableDropdown.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useMemo } from "react";
 import {AiOutlineRight} from "react-icons/ai";
+import Link from "next/link";
 const HoverableDropdown = () => {
 
 const memoizedComponent = useMemo(() => (
@@ -9,54 +10,54 @@ const memoizedComponent = useMemo(() => (
         Courses&nbsp;<AiOutlineRight className="text-slate-800  dark:text-white transform rotate-90 group-hover:rotate-[450deg] transition-transform duration-700"  />
       </button>
       <div className="absolute top-8 min-[900px]:-left-48 hidden min-[900px]:bg-black bg-slate-900 min-[500px]:w-[30rem] w-[20rem]  border-4 border-bg-gradient-to-r border-from-pink-500 border-via-red-500 border-to-yellow-500  border-gray-700 rounded-md shadow-md group-hover:block  flex_center divide-y divide-gray-200">
-        <a
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Html 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Css3 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Javascript 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Python 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Reactjs 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         Tailwindcss 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         NodeJs 
-        </a>
-        <a
+        </Link>
+        <Link
           href="/free-trial-class"
           className="dropdownMenu"
         >
         MongoDB 
-        </a>
+        </Link>
        
        
       </div>
